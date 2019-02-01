17cf60236e656fb529210efca1401a16f077fe6d
hamza0867 <hamza0867@gmail.com>
2019-02-01 14:55:04 +0100

    Pass step 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,27 @@
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
+
 User.destroy_all
+TodoList.destroy_all
 
-User.create! [
-  { username: 'Fiorina', password_digest: 'lol_Italy' },
-  { username: 'Trump', password_digest: 'we_need_WALL' },
-  { username: 'Carson', password_digest: '1_day' },
-  { username: 'Clinton', password_digest: 'I_hate_ARAFATE' }
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
+
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: 'Task 1', due_date: dates.sample, description: 'very important task TEST', completed: false },
+    { title: 'Task 2', due_date: dates.sample, description: 'do something else TEST', completed: true },
+    { title: 'Task 3', due_date: dates.sample, description: 'learn Action Pack TEST', completed: true }
+  ]
+end
+
+users = User.create! [
+  { username: 'jim', password: 'abc123' },
+  { username: 'rich', password: '123abc' }
 ]
 
-Profile.create! [
-  { first_name: 'Carly', last_name: 'Fiorina', birth_year: 1954, gender: 'female' },
-  { first_name: 'Donald', last_name: 'Trump', birth_year: 1946, gender: 'male' },
-  { first_name: 'Ben', last_name: 'Carson', birth_year: 1951, gender: 'male' },
-  { first_name: 'Hillary', last_name: 'Clinton', birth_year: 1947, gender: 'female' }
-]
-
-User.all.each do |usr|
-  usr.profile = Profile.find_by(last_name: usr.username)
-  todo_lsts = TodoList.create! [{
-    list_name: "todo list for #{usr.username}",
-    list_due_date: (Date.today + 1.year)
-  }]
-  (1..5).each do |i|
-    todo_lsts.first.todo_items << TodoItem.create!(
-      due_date: (Date.today + i.year),
-      title: 'some title',
-      description: 'Some loooooong assss description'
-    )
-  end
-  usr.todo_lists = todo_lsts
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
 end
