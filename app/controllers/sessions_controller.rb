ab0f11587314060b15711ce2fa04951a6461818d
hamza0867 <hamza0867@gmail.com>
2019-02-02 18:52:54 +0100

    Pass step 10

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+    password = params[:user][:password]
+    if user && user.authenticate(password)
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully as #{user.username}"
+    else
+      redirect_to login_path, alert: 'The username/passord combination is invalid'
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to login_path, notice: 'You have been logged out'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new', as: 'login'
+  delete '/logout' => 'sessions#destroy', as: 'logout'
+  resources :sessions, only: %i[new create destroy]
   resources :todo_lists do
     resources :todo_items
   end
